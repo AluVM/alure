60f5fb421421f67c1ca22ec912a68e672af2ba51
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-07-04 16:37:34 +0200

    Initial setup

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// AluVM Runtime Environment
+// To find more on AluVM please check <https://www.aluvm.org>
+//
+// Designed & written in 2021 by
+//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+// for Pandora Core AG
+
+fn main() {
+    println!("Hello, world!");
+}
